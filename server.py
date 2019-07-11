e296974c05f54f4e4ba79b4adaccf52f0b966be4
Anastasis Dasyras <anastasisdasy@gmail.com>
2019-07-11 13:42:01 +0300

    Changes to add stock availiability

--- server.py
+++ server.py
@@ -10,6 +10,10 @@ print(port)
 
 @app.route('/', methods=['POST'])
 def index():
+    TOMATO_VALUE = 0.75
+    CUCUMBER_VALUE = 0.5
+    CARROT_VALUE = 0.7
+    PEPPER_VALUE = 1.2
     flag = False
     # update data from thingspeak
     read_orders = requests.get(
@@ -21,7 +25,7 @@ def index():
     orders_num = orders['channel']['last_entry_id']
 
   # order product
-    if data['conversation']['memory']['inor']['value'] == 'order':
+    if data['conversation']['memory']['inor']['value'] == 'order' and data['conversation']['skill'] == 'confirm-order':
         if 'custid' in data['conversation']['memory']:
             custid = data['conversation']['memory']['custid']['raw']
             vegetable = data['conversation']['memory']['veg']['value']
@@ -40,11 +44,11 @@ def index():
                     while (not('@' in infos[3+i])):
                         location = location + ', ' + infos[3+i]
                         i = i + 1
-                    email = infos[-6]
-                    zipcode = infos[-5]
+                    email = infos[-10]
+                    zipcode = infos[-9]
                     status = 'ready to be shipped'
                     #print (infos[0]+" "+infos[1]+" "+infos[2]+" "+infos[3]+" "+infos[4]+" "+infos[5]+" "+vegetable+" "+kilos)
-                    break
+                    #break
         else:
             fullname = data['conversation']['memory']['person']['fullname']
             location = data['conversation']['memory']['location']['formatted']
@@ -67,7 +71,7 @@ def index():
         if(flag):
             my_string = orders['feeds'][int(orders_num)-1]['field1']
             infos = [x.strip() for x in my_string.split(',')]
-            previous_order = infos[-3].split('-')
+            previous_order = infos[-7].split('-')
             if(int(previous_order[1]) < 999):
                 po = [int(previous_order[0]), int(previous_order[1])+1]
             else:
@@ -80,22 +84,30 @@ def index():
                 orderid = str(po[0])+"-"+str(po[1])
 
             # create an order and save the client's info
-            final_display = 'Thank you your order will be shipped immediately \n'+str(custid)+','+fullname+','+location+','+email + \
+            final_display = str(custid)+','+fullname+','+location+','+email + \
                 ','+zipcode+','+status+','+orderid+','+vegetable+','+kilos
             payload = {'api_key': 'P7P4BWK57W19AG1J', 'field1': final_display}
             requests.post('https://api.thingspeak.com/update', params=payload)
 
     # just need information
-    else:
+    elif (data['conversation']['skill'] == 'information'):
         # search the order
         for i in range(int(orders_num)):
             my_order = orders['feeds'][i]['field1']
             infolist = [x.strip() for x in my_order.split(',')]
-            my_orderid = infolist[-3]
+            my_orderid = infolist[-7]
             if(my_orderid == data['conversation']['memory']['orderid']['value']):
-                final_display = 'Your order status: ' + infolist[-4]
+                final_display = 'Your order status: ' + infolist[-8]
                 flag = True
                 break
+    elif(data['conversation']['skill'] == 'order'):
+        #show the warehouse stock
+        my_stock = orders['feeds'][int(orders_num)-1]['field1']
+        stock_info = [x.strip() for x in my_string.split(',')]
+        tomato_stock = stock_info[-4]
+        cucumber_stock = stock_info[-3]
+        carrot_Stock = stock_info[-2]
+        pepper_Stock = stock_info[-1]
 
     # randomly change orders' status
 
